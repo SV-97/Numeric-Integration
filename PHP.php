9b7b4db42a4e83f286f541355643afe5ad09cb80
Stefan Volz <volzstefan97@googlemail.com>
2019-06-28 15:57:15 +0200

    PHP solution

---
+++ PHP.php
@@ -0,0 +1,20 @@
+<?php
+
+function composite_simpsons($f, $a, $b, $n) {
+    $step_size = ($b - $a) / $n;
+    $integral = 0;
+    for ($k=0; $k < $n; $k++) {
+        $x_k0 = $a + $step_size * $k;
+        $x_k1 = $a + $step_size  * ($k + 1);
+
+        $fac = $f($x_k0) + 4 * $f(($x_k0 + $x_k1) / 2 ) + $f($x_k1);
+        $step = $fac * $step_size / 6;
+        $integral += $step;
+    }
+    return $integral;
+}
+
+$integral = composite_simpsons(function ($x) {return sin($x);} , 0, 2 * pi(), 100000);
+echo $integral."\n"
+
+?>
