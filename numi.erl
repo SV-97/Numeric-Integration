b47e135428f269877cff2539aef2aac18b84081b
Stefan Volz <volzstefan97@googlemail.com>
2019-07-28 20:15:33 +0200

    Added Erlang

---
+++ numi.erl
@@ -0,0 +1,20 @@
+% erlc numi.erl && time escript numi.beam
+
+-module(numi).
+
+-export([compSimps/4, main/1]).
+
+simpson(F, A, Step, K) ->
+    Xk = A + K * Step,
+    Xk1 = A + (K + 1) * Step,
+    Step / 6 * (F(Xk) + 4 * F((Xk + Xk1) / 2) + F(Xk1)).
+
+compSimps(F, A, B, N) ->
+    Step = (B - A) / N,
+    lists:sum(lists:map(fun (K) -> simpson(F, A, Step, K)
+			end,
+			lists:seq(0, N))).
+
+main(_) ->
+    io:write(compSimps(fun (X) -> math:sin(X) end, 0,
+		       2 * math:pi(), 100000)).
