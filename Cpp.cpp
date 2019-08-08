3feeb331dd05e5569432f2051dbb86b8b75ff1aa
Joshua Simon <39858834+JoshuaSimon@users.noreply.github.com>
2019-08-09 01:02:12 +0200

    Fixed simpson's rule and added another example.

--- Cpp.cpp
+++ Cpp.cpp
@@ -20,18 +20,24 @@ double h (double x) {
 	return sin(x);
 }
 
+double k(double x) {
+	// Example 3: k(x) = 3^(3*x-1)
+	return pow(3, 3 * x - 1);
+}
+
 // Composite Simpson's rule
 double com_simpson (double f(double), double a, double b, int n);
 
 // Trapezoidal rule
 double trapez (double f(double), double a, double b, int n);
 
+
 /* Main */
 int main (void) {
 	
 	double interval_a = 0.0;		// lower interval bound
 	double interval_b = 4.0;		// upper interval bound
-	int n = 100;					// number of subintervals
+	int n = 100;				// number of subintervals
 	
 	// Call functions to calculate integral value
 	// Example 1:
@@ -42,6 +48,9 @@ int main (void) {
 	double integral_h_1 = com_simpson(h, 0.00, 3.141592654/2.0, 400);
 	double integral_h_2 = trapez(h, 0.00, 3.141592654/2.0, 100);
 	
+	// Example 3:
+	double integral_k_1 = com_simpson(k, 0.0, 2.0, 3);
+	double integral_k_2 = trapez(k, 0.0, 2.0, 3);
 	
 	// Output
 	cout << "Integral of g(x) over [" << interval_a << "," << interval_b << "] " << endl;
@@ -54,39 +63,43 @@ int main (void) {
 	cout << "Trapez:  " << integral_h_2 << endl;
 	cout << "Exact value is " << -cos(3.141592654/2.) + cos(0.) << " ." << endl << endl;
 	
+	cout << "Integral of k(x) over [0, 2] " << endl;
+	cout << "Simpson: " << integral_k_1 << endl;
+	cout << "Trapez:  " << integral_k_2 << endl;
+	cout << "Exact value is " << 728 / (9 * log(3)) << " ." << endl << endl;
 	
 	system("Pause");
 }
 
 // Composite Simpson's rule
-double com_simpson (double f(double), double a, double b, int n) {
-	
+double com_simpson(double f(double), double a, double b, int n) {
+
 	// Integration of function f over interval [a,b] where n is
 	// the number of subintervals. Accuracy only depends on the 
 	// number of subintervals.
 	double h, x_k, x_k1, simpson;
-	double sum = 0.00;
-	
-	if(a > b)
+	double sum = 0.0;
+
+	if (a > b)
 	{
 		cerr << "ERROR: Lower interval bound a has to be smaller than upper bound b." << endl;
 		return 0;
 	}
-	
+
 	// Step length h
 	h = (b - a) / n;
-	
+
 	// Calculate integral value with composite simpson's rule
-	for ( int k = 0; k <= n; k++)
+	for (int k = 1; k <= n; k++)
 	{
 		x_k = a + k * h;
-		x_k1 = a + (k+1) * h;
-		
-		simpson = h/6 * ( f(x_k) + 4* f((x_k + x_k1)/2) + f(x_k1));
-		
+		x_k1 = a + (k - 1.0) * h;
+
+		simpson = h / 6 * (f(x_k1) + 4 * f((x_k1 + x_k) / 2) + f(x_k));
+
 		sum = sum + simpson;
 	}
-	
+
 	return sum;
 }
 
